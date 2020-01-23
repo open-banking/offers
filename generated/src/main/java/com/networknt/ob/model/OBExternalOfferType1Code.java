34ff06d0623fd216b3c57fe934e39b6c541660b0
Steve Hu <stevehu@gmail.com>
2020-01-23 14:45:46 -0500

    fixes #1 scaffold project with specification and light-codegen

---
+++ generated/src/main/java/com/networknt/ob/model/OBExternalOfferType1Code.java
@@ -0,0 +1,8 @@
+
+package com.networknt.ob.model;
+import java.util.Enumeration;
+
+public enum OBExternalOfferType1Code {
+    BalanceTransfer, LimitIncrease, MoneyTransfer, Other, PromotionalRate;
+
+}
